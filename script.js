b2044e2c7d99557aa70be107ba8d1832d44a9fab
Mohammad Saber Mahmud Sourav <74147777+sabermh46@users.noreply.github.com>
2023-02-21 18:27:07 +0600

    update

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 
+var float1 = document.querySelector('.floating1')
+var stretch1 = document.querySelector('.stretch1')
+var links = document.querySelectorAll('.links a img');
+var brand = document.querySelector('.brand')
 
+function GenerateLinks(){
 
-function float1(){
-    var float1 = document.querySelector('.floating1')
+    brand.style.transform = 'translate(0px, 0px)'
+
+    links.forEach((link, i) => {
+        link.style.transitionDelay = `${0.8 + i * 0.1}s`
+        link.style.translate = '0px 0px'
+    }).then((e)=> {
+        links.forEach((link) => {
+            link.style.transitionDelay = `0s`
+        })
+    })
 }
 
-window.onscroll = (e) => {
-    console.log(e.scrollY, e.sc)
+
+function dofloat1(y){
+    float1.style.rotate = `${y*0.2}deg`
+    float1.style.translate = `${y*0.4}px ${y*0.6}px`
 }
+function doStretch(y){
+    stretch1.style.height = `${10 + y*0.5}px`
+}
+
+function allScrollElements(){
+    window.onscroll = (e) => {
+        var y = window.scrollY
+        dofloat1(y)
+        doStretch(y)
+        console.log(y)
+    }
+}
+
+
+
+window.addEventListener('DOMContentLoaded', (e)=> {
+
+    allScrollElements()
+    GenerateLinks()
+
+})
+
